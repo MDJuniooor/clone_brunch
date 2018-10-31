935c49cf329741fcc68e409b5e455a0a6a4a8f96
mdjuniooor <mmdsds@naver.com>
2018-10-31 16:21:18 +0900

    view 최종 수정

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from .forms import PostForm, CustomUserForm
-from .models import CustomUser, Post
+from .models import CustomUser, Post, Comment
+
 
 def index(request):
     return render(request, 'blog/index.html')
+
 
 def register(request):
     if request.user.is_authenticated:
@@ -42,14 +45,17 @@ def signin(request):
         form = CustomUserForm()
     return render(request, 'blog/signin.html', {'form':form, 'error_msg':error_msg})
 
-def signout(request):
 
+
+def signout(request):
+    logout(request)
     return render(request, 'blog/signout.html')
 
 
 def create_post(request):
     if not request.user.is_authenticated:
         return redirect('index')
+
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid(): # author auto-field
@@ -61,6 +67,7 @@ def create_post(request):
         form = PostForm()
     return render(request, 'blog/create_post.html', {'form':form})
 
+
 def my_post(request):
     if not request.user.is_authenticated:
         return redirect('index')
@@ -71,4 +78,58 @@ def my_post(request):
 
 def show_post(request, post_id):
     post = Post.objects.get(id=post_id)
-    return render(request, 'blog/show_post.html', {'post': post})
+    comments = Comment.objects.filter(post=post)
+    return render(request, 'blog/show_post.html', {'post': post, 'comments': comments})
+
+def delete_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+
+    if request.user != post.author:
+        return redirect('index')
+
+    post.delete()
+    return redirect('my_post')
+
+def update_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+
+    if request.user != post.author:
+        return redirect('index')
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('my_post')
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/update_post.html', {'form': form})
+
+
+def subscribe(request, author_name):
+    if not request.user.is_authenticated:
+        return redirect('index')
+    author = CustomUser.objects.get(name=author_name)
+    request.user.following.add(author)
+    return redirect('index')
+
+def list_following(request, user_name):
+    user_obj = CustomUser.objects.get(name=user_name)
+    following = list(user_obj.following.all())
+    return render(request, 'blog/following.html', {'following':following})
+
+def list_post(request, user_name):
+    user_obj = CustomUser.objects.get(name=user_name)
+    posts = Post.objects.filter(author=user_obj).order_by('-date')
+    return render(request, 'blog/list_post.html', {'posts':posts})
+
+def search_post(request):
+    posts = Post.objects.all()
+    q = request.GET.get('q','')
+    if q:
+        posts = posts.filter(Q(title__icontains=q)
+                             | Q(contents__icontains=q)
+                             | Q(author__name__icontains = q)
+        )
+    return render(request, 'blog/search_post.html', {'posts':posts})
