3c95badebd7e61f574a6e428ec015cd85e22b2c9
mdjuniooor <mmdsds@naver.com>
2018-11-01 17:36:55 +0900

    deploy -- 2

--- brunch/settings.py
+++ brunch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '2@4zx-$g&^58b&)x2zsktx0k=*sk$3!o2w%sq0km=luf-k&+(k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
